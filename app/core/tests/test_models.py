bcab4eaabbb5f2a17eeab62e220c00a177410fa4
Noelia <noelia@MacBook-Air-de-Noelia.local>
2023-03-13 11:27:33 -0300

    wip

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,13 +9,13 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
     """Test models."""
 
-    def test_create_user_with_email_successfully(self):
+    def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful"""
         email = 'test@example.com'
         password = 'testpass123'
 
         user = get_user_model().objects.create_user(
-            email=email,
+            email=self.normalize_email(email),
             password=password,
         )
 
