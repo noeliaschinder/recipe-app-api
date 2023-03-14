0010bb9d29e4ee94ee48a1b24af3a8d92b24b152
Noelia <noelia@MacBook-Air-de-Noelia.local>
2023-03-14 16:37:14 -0300

    fixing ut

--- app/core/models.py
+++ app/core/models.py
@@ -21,9 +21,9 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, password=None):
-        """Create, save and return a new super user."""
-        user = self.create_user(email=email, password=password)
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
